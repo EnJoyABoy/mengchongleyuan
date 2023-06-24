99cf8188f386a5cc9c7f04cef906086c2ba59abd
li <2689262623@qq.com>
2023-06-24 23:29:05 +0800

    实现注册功能

--- mengchongleyuan/apps/users/views.py
+++ mengchongleyuan/apps/users/views.py
@@ -1,21 +1,73 @@
 from django.shortcuts import render, redirect, reverse
 from django.views import View
+from django import http
+from apps.users.models import User
+import re
+import logging
 
 
+logger = logging.getLogger('django')
 # Create your views here.
 # 注册视图
 class RegisterView(View):
 
     def get(self, request):
-        # path = reverse('users:register')
-        # return redirect(path)
         return render(request, 'register.html')
+
+    def post(self, request):
+        data = request.POST
+        username = data.get('username')
+        passwrod = data.get('password')
+        passwrod2 = data.get('password2')
+        mobile = data.get('mobile')
+        # 数据的验证
+        # 验证必传的数据是否有值 all([el,el,el]) el必须有值 只要有一个为None 则为False
+        if not all([username, passwrod, passwrod2, mobile]):
+            return http.HttpResponseBadRequest('参数有问题')
+        # 判断用户名是否符合规则 判断 5-20位 数字 字母 _
+        if not re.match(r'[0-9a-zA-Z_]{5,20}', username):
+            return http.HttpResponseBadRequest('用户名不合法')
+        # 判断密码是否符合规则
+        if not re.match(r'[0-9a-zA-Z_]{8,20}', passwrod):
+            return http.HttpResponseBadRequest('密码不合法')
+        # 判断确认密码和密码是否一致
+        if passwrod != passwrod2:
+            return http.HttpResponseBadRequest('密码不一致')
+        # 判断手机号是否符合规则
+        if not re.match(r'1[3-9]\d{9}', mobile):
+            return http.HttpResponseBadRequest('手机号不符合规则')
+
+        # 验证数据没有问题才入库
+        try:
+            user = User.objects.create_user(username=username, password=passwrod, mobile=mobile)
+        except Exception as e:
+            logger.error(e)
+            return http.HttpResponseBadRequest('数据库异常')
+
+        # 注册完成之后,默认认为用户已经登陆 保持登陆的状态
+        # 系统也能自己去实现登陆状态的保持
+        from django.contrib.auth import login
+        login(request, user)
+
+        # 跳转到首页
+        return redirect(reverse('contents:index'))
+
 
 # 登入视图
 class LoginView(View):
 
     def get(self, request):
-        # path = reverse('users:login')
-        # return redirect(path)
-
         return render(request, 'login.html')
+
+# 在注册时Vue发起ajax请求验证数据库中是否有相同用户名
+class UsernameCountView(View):
+
+    def get(self,request,username):
+        # 查询数据库,通过查询记录的count来判断是否重复 0表示没有重复 1表示重复
+        try:
+            count = User.objects.filter(username=username).count()
+        except Exception as e:
+            logger.error(e)
+            return http.JsonResponse({'code':400,'errmsg':'数据库异常'})
+        # 返回相应
+        return http.JsonResponse({'code':0,'count':count})
---
+++ mengchongleyuan/static/js/register.js
@@ -0,0 +1,105 @@
+var vm = new Vue({
+    el: '#app',
+    // 修改Vue变量的读取语法，避免和django模板语法冲突
+    delimiters: ['[[', ']]'],
+    data: {
+        host: host,
+        error_name: false,
+        error_password: false,
+        error_check_password: false,
+        error_mobile: false,
+        error_image_code: false,
+        error_sms_code: false,
+        error_allow: false,
+        error_name_message: '请输入5-20个字符的用户',
+        error_password_message: '请输入8-20位的密码',
+        error_password2_message: '两次输入的密码不一致',
+        error_mobile_message: '请输入正确的手机号码',
+        error_image_code_message: '请填写图形验证码',
+        error_sms_code_message: '请填写短信验证码',
+        error_allow_message: '请勾选用户协议',
+        image_code_id: '',
+        image_code_url: '',
+        sms_code_tip: '获取短信验证码',
+        sending_flag: false,
+        username: '',
+        password: '',
+        password2: '',
+        mobile: '',
+        image_code: '',
+        sms_code: '',
+        allow: true
+    },
+    methods: {
+        // 检查用户名
+        check_username: function () {
+            // alert('验证用户名');
+            var re = /^[a-zA-Z0-9_-]{5,20}$/;
+            if (re.test(this.username)) {
+                this.error_name = false;
+            } else {
+                this.error_name_message = '请输入5-20个字符的用户名';
+                this.error_name = true;
+
+            }
+
+        //    在这里发送一个axios 请求
+        //    1.组织url
+            let url = '/usernames/'+this.username+'/count/';
+        //    2.发送请求
+            axios.get(url).then(response=>{
+                //    3.请求成功的回调的业务逻辑
+                // console.log(response)
+                if(response.data.count == 0){
+                    this.error_name=false
+                }else{
+                    this.error_name=true;
+                    this.error_name_message='用户名已注册';
+                }
+            }).catch(error=>{
+
+            })
+        },
+        // 检查密码
+        check_password: function () {
+            var re = /^[0-9A-Za-z]{8,20}$/;
+            if (re.test(this.password)) {
+                this.error_password = false;
+            } else {
+                this.error_password = true;
+            }
+        },
+        // 确认密码
+        check_password2: function () {
+            if (this.password != this.password2) {
+                this.error_check_password = true;
+            } else {
+                this.error_check_password = false;
+            }
+        },
+        // 检查手机号
+        check_mobile: function () {
+            var re = /^1[345789]\d{9}$/;
+            if (re.test(this.mobile)) {
+                this.error_mobile = false;
+            } else {
+                this.error_mobile_message = '您输入的手机号格式不正确';
+                this.error_mobile = true;
+            }
+        },
+        // 表单提交
+        on_submit(){
+            this.check_username();
+            this.check_password();
+            this.check_password2();
+            this.check_mobile();
+
+            if (this.error_name == true || this.error_password == true || this.error_check_password == true
+                || this.error_mobile == true) {
+                // 不满足注册条件：禁用表单
+                window.event.returnValue = false;
+            }
+        }
+
+    }
+});
