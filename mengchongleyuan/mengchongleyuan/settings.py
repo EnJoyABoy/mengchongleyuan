732eba77da3c81ed90368ee783b896381bb7e27d
li <2689262623@qq.com>
2023-06-25 17:12:53 +0800

    创建地址表格迁移并导入数据，数据在.sql表中
    迁移：python manage.py makemigrations和python manage.py migrate

--- mengchongleyuan/mengchongleyuan/settings.py
+++ mengchongleyuan/mengchongleyuan/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.contents.apps.ContentsConfig',
     'apps.users.apps.UsersConfig',
+    'apps.areas.apps.AreasConfig',
 ]
 
 MIDDLEWARE = [
