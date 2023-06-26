e0dcfb9dc2d3ab9f8488d6ec1e72753acb7b2df7
li <2689262623@qq.com>
2023-06-26 23:56:27 +0800

    创建发布需求模型并实现发布功能

--- mengchongleyuan/apps/blogs/models.py
+++ mengchongleyuan/apps/blogs/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Blogs(models.Model):
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='blogs', verbose_name='用户')
+    province = models.ForeignKey('areas.Area', on_delete=models.CASCADE, related_name='province_addresses', verbose_name='省')
+    city = models.ForeignKey('areas.Area', on_delete=models.CASCADE, related_name='city_addresses', verbose_name='市')
+    district = models.ForeignKey('areas.Area', on_delete=models.CASCADE, related_name='district_addresses', verbose_name='区')
+    address = models.CharField(max_length=200, verbose_name='详细地址')
+    contents = models.CharField(max_length=200, verbose_name='需求')
+    times = models.CharField(max_length=200, verbose_name='时间')
+
+    class Meta:
+        db_table = 'tb_blogs'
+        verbose_name = '需求博客'
+        verbose_name_plural = verbose_name
+
--- mengchongleyuan/apps/blogs/views.py
+++ mengchongleyuan/apps/blogs/views.py
@@ -1,12 +1,54 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django import http
+from apps.blogs.models import Blogs
+import logging
+from apps.areas.models import Area
 
+
+logger = logging.getLogger('django')
 class CewateblogsView(LoginRequiredMixin, View):
 
     # 创建博客需求
     def get(self, request):
         return render(request, 'create_blogs.html')
+
+    def post(self, request):
+        # 接收数据
+        data = request.POST
+        contents = data.get('blogs')
+        times = data.get('blogs_time')
+        province = data.get('province')
+        city = data.get('city')
+        district = data.get('district')
+        address = data.get('blogs_address')
+
+        # 验证数据
+        if not all([contents, times, province, city, district, address]):
+            return http.HttpResponseBadRequest('缺少必传参数')
+
+        province = Area.objects.filter(id=province)
+        city = Area.objects.filter(id=city)
+        district = Area.objects.filter(id=district)
+
+        # 数据入库
+        try:
+            blogs = Blogs.objects.create(
+                user=request.user,
+                province=province[0],
+                city=city[0],
+                district=district[0],
+                address=address,
+                contents=contents,
+                times=times
+            )
+        except Exception as e:
+            logger.error(e)
+            return http.HttpResponseBadRequest('数据库异常')
+
+        # 跳转到首页
+        return redirect(reverse('contents:index'))
+
+
+
