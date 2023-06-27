9b402250003f3573c58642c09cd07f56065181d6
li <2689262623@qq.com>
2023-06-27 10:56:51 +0800

    首页显示数据库中读取的需求博客内容

--- mengchongleyuan/apps/contents/views.py
+++ mengchongleyuan/apps/contents/views.py
@@ -3,10 +3,26 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 from django.views import View
+from apps.blogs.models import Blogs
 
 
 class IndexView(View):
 
     # 主页面展示
     def get(self, request):
-        return render(request, 'index.html')
+        blogs = Blogs.objects.filter()
+        blog_list = []
+        for blog in blogs:
+            blog_list.append({
+                'contents': blog.contents,
+                'times': blog.times,
+                'province': blog.province,
+                'city': blog.city,
+                'district': blog.district,
+                'address': blog.address,
+                'user': blog.user
+            })
+        context = {
+            'blogs': blog_list
+        }
+        return render(request, 'index.html', context=context)
